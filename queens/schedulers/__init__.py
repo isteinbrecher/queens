f99cfe1bee852e9d674fd9ddcf70544f0df3ac38
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-12-06 14:55:22 +0100

    Add local cluster for charon

--- queens/schedulers/__init__.py
+++ queens/schedulers/__init__.py
@@ -7,6 +7,7 @@ system call.
 
 from queens.schedulers.cluster_scheduler import ClusterScheduler
 from queens.schedulers.local_cluster_scheduler import LocalClusterScheduler
+from queens.schedulers.local_cluster_scheduler_charon import CharonLocalCluster
 from queens.schedulers.local_scheduler import LocalScheduler
 from queens.schedulers.pool_scheduler import PoolScheduler
 from queens.schedulers.scheduler import Scheduler
@@ -15,5 +16,6 @@ VALID_TYPES = {
     "local": LocalScheduler,
     "cluster": ClusterScheduler,
     "local_cluster": LocalClusterScheduler,
+    "local_cluster_charon": CharonLocalCluster,
     "pool": PoolScheduler,
 }
